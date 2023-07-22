f5b939134f86ebc27f8958f747c814fb9b91961e
toftpokk <pakkapol.20100@gmail.com>
2023-07-22 17:09:47 +0700

    Feat: move dir listing to show_subdir

--- api.js
+++ api.js
@@ -3,6 +3,21 @@ import {readdir,lstat} from 'fs/promises'
 import { assets_dir } from './env.js'
 export const router = express.Router()
 
+const show_subdir = async (path)=>{
+    let isdir = false;
+    let dir_items = [];
+    try {
+        isdir = (await (lstat(path))).isDirectory()
+    }
+    catch(e){
+        console.log(`Could not read path ${path}\n${e}`)
+    }
+    if(isdir){
+        dir_items = await readdir(path)
+    }
+    return dir_items
+}
+
 router.get('/',(req,res)=>{
     return res.json({
         message: "Hello World!"
@@ -10,7 +25,7 @@ router.get('/',(req,res)=>{
 })
 
 router.get('/repo',async (req,res)=>{
-    const repos = await readdir(assets_dir)
+    const repos = await show_subdir(assets_dir)
     return res.json({
         repos
     })
@@ -18,7 +33,7 @@ router.get('/repo',async (req,res)=>{
 
 router.get('/repo/:repo',async (req,res)=>{
     const repo = req.params.repo
-    const dirs = await readdir(assets_dir+'/'+repo)
+    const dirs = await show_subdir(assets_dir+'/'+repo)
     return res.json({
         repo,
         dirs
@@ -28,7 +43,7 @@ router.get('/repo/:repo',async (req,res)=>{
 router.get('/repo/:repo/:dir',async (req,res)=>{
     const repo = req.params.repo
     const dir = req.params.dir
-    const subdirs = await readdir(assets_dir+'/'+repo+'/'+dir)
+    const subdirs = await show_subdir(assets_dir+'/'+repo+'/'+dir)
     return res.json({
         repo,
         dir,
@@ -41,14 +56,7 @@ router.get('/repo/:repo/:dir/:subdir',async (req,res)=>{
     const dir = req.params.dir
     const subdir = req.params.subdir
     let is_404 = false
-    let items = []
-    const isdir = (await (lstat(assets_dir+'/'+repo+'/'+dir+'/'+subdir))).isDirectory()
-    if(isdir){
-        items = await readdir(assets_dir+'/'+repo+'/'+dir+'/'+subdir)
-    }
-    else{
-        is_404 = true
-    }
+    let items = await show_subdir(assets_dir+'/'+repo+'/'+dir+'/'+subdir)
     return res.status(is_404?404:200).json({
         repo,
         dir,
